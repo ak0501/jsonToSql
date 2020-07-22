2af81b7923b4df59fb04d96ef5f315fe1b46e740
khab0501 <48302535+khab0501@users.noreply.github.com>
2020-07-21 21:36:36 -0500

    installed sql module locally

--- config/connection.js
+++ config/connection.js
@@ -29,7 +29,7 @@ connection=mysql.createConnection({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
-        port: 3306,
+        // port: 3306,
         database:"notes_db"
 });
 }
